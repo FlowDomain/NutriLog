123a4ab5ed4a091beb1e24cdf5762c782751f606
FlowDomain <flowdomain85@gmail.com>
2025-11-06 23:22:48 +0530

    Post Deployment fixes

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -17,6 +17,16 @@ export const getAuth = async () => {
         database: mongodbAdapter(db as any),
         secret: process.env.BETTER_AUTH_SECRET,
         baseURL: process.env.BETTER_AUTH_URL,
+
+        cors: {
+            origin: [
+                "http://localhost:3000",
+                process.env.NEXT_PUBLIC_APP_URL!,   // your deployed domain
+            ],
+            methods: ["GET", "POST", "OPTIONS"],
+            allowedHeaders: ["Content-Type", "Authorization"],
+            credentials: true,
+        },
         emailAndPassword: {
             enabled: true,
             disableSignUp: false,
